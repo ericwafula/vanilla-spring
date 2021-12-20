c626d1d231048feec39510d7959295e47cb96484
ericwafula <wathomeeric@gmail.com>
2021-12-20 20:19:21 +0300

    initial commit

---
+++ src/main/java/tech/ericwathome/vanillaspring/VanillaSpringApplication.java
@@ -0,0 +1,13 @@
+package tech.ericwathome.vanillaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanillaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/ericwathome/vanillaspring/VanillaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ericwathome.vanillaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanillaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
